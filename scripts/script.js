e937ff77b46e17cb9715ca644c8a0c7856f4048d
Gabriel Rocha <rochagbriel@gmail.com>
2023-03-30 01:00:56 +0200

    Add: Added a crossOut function that crosses out the item after a double click event.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 function newItem () {
+
     // Add a new item to the list of items:
+
     let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
@@ -9,5 +11,15 @@ function newItem () {
     } else {
         $('#list').append(li);
     }
+
+    // Crossing out an item from the list of items:
+
+    function crossOut() {
+        li.toggleClass('strike');
+    }
+
+    li.on('dblclick', function crossOut() {
+        li.toggleClass('strike');
+    });
 
 }
